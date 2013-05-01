920b7a75437da6537950ea06e33e5998f2d87041
dhoko <aurelien@procheo.fr>
2013-05-01 19:27:34 +0200

    update configuration

--- config.php
+++ config.php
@@ -14,27 +14,37 @@ $cyaneus = array(
 	'author' => '',
 	// SIte description
 	'description' => '',
+	// Template Name
+	'template_name' => 'base',
+	// Thumbnail width
+	'thumb_w' => 600,
+	// Date format to display
+	'date_format' => 'd/m/Y',
 
 	/**
 	 * Cyaneus Confiuration
 	 */
 	
 	// Tags you want to edit in your posts
-	'tags' => 'title,url,date,tags,description,author',
+	'tags' => '',
 	// The generator
 	'generator' => 'Cyaneus 1.0 B2',
 	// Put your drafts in this folder
 	'draft' => 'draft',
 	// Your posts builds will be store in this folder
 	'articles' => 'articles',
-	// Thumbnail width
-	'thumb_w' => 600,
+	// Template folder
+	'template' => 'template',
+	// To change the destination of your site.
+	// . = Same folder as your config.php
+	'folder_main_path' => '.',
+
 
 	/**
 	 * Rebuild Key
 	 * You can rebuild your site online but to prevent other to do it you must set a password here.
 	 */
-	'rebuild_key' => '',
+	'rebuild_key' => 'test',
 
 	/**
 	 * Git Hook Mechanism
