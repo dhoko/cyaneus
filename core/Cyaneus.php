ce7a435f8b66207d48e38bb3210e8f5a50c5e0de
dhoko <aurelien@procheo.fr>
2013-05-19 23:01:16 +0200

    Delete du vieux systeme de DB

--- core/Cyaneus.php
+++ core/Cyaneus.php
@@ -5,30 +5,6 @@ class Cyaneus {
 	protected $pictFilesExt = array('jpg','jpeg','gif','webp','png','bmp','ico');
 	protected $db = array();
 
-	// protected function update(Array $db) {
-	// 	$postConfig = USERDATA.DIRECTORY_SEPARATOR.'posts.json';
-
-	// 	if(file_exists($postConfig)) {
-	// 		$data = file_get_contents($postConfig);
-	// 		if ($data) {
-	// 			$info = json_decode(base64_decode($data),true);
-	// 			$info[key($db)] = $db[key($db)];
-	// 			$update = base64_encode(json_encode($info));
-	// 			file_put_contents($postConfig,$update);
-	// 			$this->db = $info;
-	// 			klog('Update DB successfully');
-	// 			return true;
-	// 		}else{
-	// 			klog('Cannot update DB for '.serialize($db),"error");
-	// 			return false;
-	// 		}
-	// 	}else{
-	// 		file_put_contents($postConfig,base64_encode(json_encode($db)));
-	// 		klog('Create DB successfully');
-	// 	}
-		
-	// }
-
 	/**
 	 * Record data to Posts and Picture table
 	 * @param  Array $db list of files [pict,post]
