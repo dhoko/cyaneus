afe820c285c34c84569da55c9da0137dcaa69832
dhoko <aurelien@procheo.fr>
2013-05-27 00:06:12 +0200

    Commande de base

--- console.php
+++ console.php
@@ -1,5 +1,6 @@
 <?php 
 require_once __DIR__.'/core/autoload.php';
+require_once __DIR__.'/core/Factory.php';
 
 use Symfony\Component\Console as Console;
 
--- core/Commands/CreateCommand.php
+++ core/Commands/CreateCommand.php
@@ -12,12 +12,15 @@ class CreateCommand extends Console\Command\Command {
 	    $this->setHelp('Outputs post title message.');
 	    $this->addArgument('name', Console\Input\InputArgument::OPTIONAL, 'The name of your post', 'Post title here');
 	    $this->addOption('more', 'm', Console\Input\InputOption::VALUE_NONE, 'Tell me more');
+	    $this->addOption('markdown', 'M', Console\Input\InputOption::VALUE_OPTIONAL, 'Post format',true);
 	}
 
     protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output) {
     	$name = $input->getArgument('name');
 
+
         $output->writeln(sprintf('Your post title:  %s', $name));
+        $output->writeln(sprintf('Your post file:  %s', \Factory::url($name).'.md'));
         if ($input->getOption('more')) {
             $output->writeln('More options informations');
         }
