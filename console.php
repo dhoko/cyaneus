840f27d4958e56546c4e40da2590e830df92b782
dhoko <aurelien@procheo.fr>
2013-06-23 20:00:50 +0200

    init commit without console

--- console.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once __DIR__.'/core/autoload.php';
-require_once __DIR__.'/core/Factory.php';
-
-use Symfony\Component\Console as Console;
-
-$application = new Console\Application('cyaneus', '1.0.0');
-$application->add(new Commands\CreateCommand('create'));
-$application->run();
