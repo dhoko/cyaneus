6decf2a1a2364708b5d67944b73c786391eb11a7
dhoko <aurelien@procheo.fr>
2013-11-27 17:03:00 +0100

    Add models

---
+++ Cyaneus/Template/Models/Page.php
@@ -0,0 +1,51 @@
+<?php
+namespace Cyaneus\Template\Models;
+use Cyaneus\Template\Models\AbstractTemplateModel;
+use Cyaneus\Cyaneus;
+use Cyaneus\Helpers\CDate;
+
+/**
+ * Model for a Page
+ */
+class Page extends AbstractTemplateModel
+{
+
+    public function build()
+    {
+        return $this->makePages();
+    }
+
+    private function makePages() {
+
+        $content = '';
+        $_tags   = [];
+        $_tags = $this->getTags(['content' => $this->makePosts()]);
+        $_tags['navigation'] = $this->navigation;
+
+        return $this->bindParams($this->template['main'],$_tags);
+    }
+
+    /**
+     * Build the content for a post
+     * @return Array list of tags per post
+     */
+    private function makePosts()
+    {
+        $content = '';
+        $_tags   = [];
+
+        foreach ($this->posts as $post) {
+
+            $_tags[$post['config']['added_time']] = $this->getTags($this->buildCustomtags($post['config'],$post['text']));
+        }
+
+        // To be sure that we order from the latest
+        ksort($_tags);
+
+        foreach ($_tags as $tags) {
+            $content .= $this->bindParams($this->template['content'],$tags);
+        }
+        return $content;
+    }
+
+}
